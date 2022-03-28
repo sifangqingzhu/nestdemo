a57ca0455d97a2d74c6b7117811014ba01b9812b
sifangqinghu <893232553@qq.com>
2022-03-28 21:59:51 +0800

    【U】 1. 还是练习

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return '你好 nest';
+  }
+
+  getNews(): string {
+    return '这个是news字符串';
   }
 }
